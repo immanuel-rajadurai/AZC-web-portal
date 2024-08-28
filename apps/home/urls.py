271a9203423412c441d4715e41e2220e73398649
Inigo Cojuangco <inigo.cojuangco0831@gmail.com>
2024-08-28 02:40:06 +0100

    auto format files

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -11,7 +11,9 @@ urlpatterns = [
     # The home page
     path('', views.index, name='home'),
     path('events/', views.events, name='events'),
+
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
+
 
 ]
