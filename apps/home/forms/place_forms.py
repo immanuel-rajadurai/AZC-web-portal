a4100c41914e7a3f66f7e3f1ebcc0d244a46e0de
Immanuel Rajadurai <immanuel.rajadurai@kcl.ac.uk>
2024-09-28 18:25:26 +0100

    fix forms and image display

--- apps/home/forms/place_forms.py
+++ apps/home/forms/place_forms.py
@@ -4,5 +4,5 @@ from django import forms
 class PlaceForm(forms.Form):
     name = forms.CharField(max_length=100)
     description = forms.CharField(widget=forms.Textarea)
-    isOpen = forms.BooleanField(initial=True)
+    isOpen = forms.BooleanField(initial=True, required=False)
     image = forms.CharField(max_length=500, required=False)
