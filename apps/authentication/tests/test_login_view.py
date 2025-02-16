e99b48cb84ef9364e37d947e5392fb6896de8507
Inigo Cojuangco <inigogabrielcojuangco@gmail.com>
2025-02-16 16:30:49 +0000

    create tests for login view

--- apps/authentication/tests/test_login_view.py
+++ apps/authentication/tests/test_login_view.py
@@ -1,10 +1,19 @@
 from django.test import TestCase
 
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class login_viewTestCase(TestCase):
     def setUp(self):
         self.url = reverse('login')
+        
+        User.objects.create_user(
+            username="johndoe",
+            email='johndoe@example.org',
+            password='Password123'
+        )
+        
+        self.user = User.objects.get(email='johndoe@example.org')
     
     def test_login_url(self):
         self.assertEqual(self.url, '/login/')
@@ -21,3 +30,20 @@ class login_viewTestCase(TestCase):
         response = self.client.get(self.url)
         self.assertIn('form', response.context)
         self.assertIn('msg', response.context)
+        
+    def test_msg_is_none(self):
+        response = self.client.get(self.url)
+        self.assertIs(response.context['msg'], None)
+        
+    def test_msg_is_not_none(self):
+        response = self.client.post(self.url, {'username': 'test', 'password': 'test'})
+        self.assertIsNot(response.context['msg'], 'Error validating the form')
+        
+    def test_form_is_valid(self):
+        response = self.client.post(self.url, {'username': 'test', 'password': 'test'})
+        self.assertIsNot(response.context['msg'], 'Invalid credentials')
+        
+    def test_valid_credentials(self):
+        response = self.client.post(self.url, {'username': 'johndoe', 'password': 'Password123'})
+        self.assertEqual(response.status_code, 302)
+        self.assertIs(response.context['msg'], None)
