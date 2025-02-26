b9c03875f70c1537b9753e7f840af0d0198e3199
Inigo Cojuangco <inigocojuangco@MacBook-Pro.local>
2025-02-26 01:34:50 +0000

    Update test_animal_place_services.py

--- apps/home/tests/services/test_animal_place_services.py
+++ apps/home/tests/services/test_animal_place_services.py
@@ -7,10 +7,13 @@ class AnimalPlaceServices(TestCase):
         self.service = AnimalPlaceServices()
         
     def test_add_animal_to_place(self):
+        
         self.service.add_animal_to_place('animal_id', 'place_id')
+        
         
     def test_get_animals_linked_to_place(self):
         self.service.get_animals_linked_to_place('place_id')
+
         
     def test_remove_animal_from_place(self):
         self.service.remove_animal_from_place('animal_id', 'place_id')
