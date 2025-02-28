e32d9db4327722c3aa16e15142f7a249c289bbc5
Inigo Cojuangco <inigogabrielcojuangco@gmail.com>
2025-02-28 21:26:16 +0000

    added to test suite
    - completed tests for template tags

---
+++ apps/home/tests/templatetags/test_miscellaneous_extras.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+from ...templatetags import miscellaneous_extras
+
+
+class MiscellaneousExtrasTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_item(self):
+        self.assertEqual(miscellaneous_extras.get_item({'key': 'value'}, 'key'), 'value')
---
+++ apps/home/tests/templatetags/test_place_extras.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+
+from ...templatetags import place_extras
+from ...services import place_services
+
+
+class PlaceExtrasTestCase(TestCase):
+    def delete_test_place(self):
+        places = place_services.get_animals_list()
+        place = next((place for place in places if place['name'] == 'TEST'), None)
+        place_services.delete_place(place['id']) if place else None
+
+    def get_test_place(self):
+        places = place_services.get_places_list()
+        return next((place for place in places if place['name'] == 'TEST'), None)
+    
+    def setUp(self):
+        place_services.add_place('TEST', 'testing description', 'https://www.google.com')
+
+    def tearDown(self):
+        super().tearDown()
+        self.delete_test_animal()
+
+    def test_get_place_name(self):
+        place = self.get_test_place()
+        self.assertIsNotNone(place)
+
+        self.assertEqual(place_extras.get_place_name(place['id']), 'TEST')
+
+    def test_get_place_name_undefined(self):
+        self.assertEqual(place_extras.get_place_name(1234), 'Undefined')
